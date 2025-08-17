92166b928ac85646a94ed9180d406694ed502f54
Manoon <manon.lagniel@gmail.com>
2025-08-17 10:13:22 +0200

    creation of agency crud

--- tests/Controller/AgencyControllerTest.php
+++ tests/Controller/AgencyControllerTest.php
@@ -2,15 +2,112 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Agency;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class AgencyControllerTest extends WebTestCase
 {
+    private KernelBrowser $client;
+    private EntityManagerInterface $manager;
+    private EntityRepository $agencyRepository;
+    private string $path = '/agency/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->agencyRepository = $this->manager->getRepository(Agency::class);
+
+        foreach ($this->agencyRepository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+
+        $this->manager->flush();
+    }
+
     public function testIndex(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/agency');
+        $this->client->followRedirects();
+        $crawler = $this->client->request('GET', $this->path);
 
-        self::assertResponseIsSuccessful();
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Agency index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first()->text());
+    }
+
+    public function testNew(): void
+    {
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'agency[city]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects($this->path);
+
+        self::assertSame(1, $this->agencyRepository->count([]));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Agency();
+        $fixture->setCity('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Agency');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Agency();
+        $fixture->setCity('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'agency[city]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/agency/');
+
+        $fixture = $this->agencyRepository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getCity());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Agency();
+        $fixture->setCity('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertResponseRedirects('/agency/');
+        self::assertSame(0, $this->agencyRepository->count([]));
     }
 }

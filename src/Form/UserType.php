274ad259f1636a7be6874a96d8830f2905dc390e
Manoon <manon.lagniel@gmail.com>
2025-08-17 10:41:18 +0200

    create users crud

---
+++ src/Form/UserType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('phone')
+            ->add('email')
+            ->add('password')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN', 
+                    'User' => 'ROLE_USER',
+                ],
+                'multiple' => false,
+                'expanded' => true,
+            ])
+        ;
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($roleString) {
+                    return $roleString ? [$roleString] : [];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
